ab0f0fe5d46f3cb5b8b04467eb2ecfe1af6e189c
Aleena C B <184249015+aleenacb@users.noreply.github.com>
2026-03-03 23:04:41 +0530

    Create swap.py

---
+++ Python/swap.py
@@ -0,0 +1,13 @@
+n = 42
+f = 3.14
+s = "Hello, World!"
+li = [1, 2, 3]
+d = {'key': 'value'}
+b = True
+
+print(type(n))   
+print(type(f)) 
+print(type(s))   
+print(type(li))     
+print(type(d))     
+print(type(b))
